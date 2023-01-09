4bbac208cb018becf92a9b0d3900a260d91e9cde
jasondansie <jasondansie@gmail.com>
2023-01-09 12:16:05 +0200

    adds searc to recipes

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -5,31 +5,57 @@ import classes from './Recipes.module.css'
 
 const Recipes = () => {
     const [data, setData] = useState([]);
+    const [recipeList, setRecipeList] = useState([]);
+    const [search, setSearch] = useState({
+        string: '',
+    });
 
     useEffect(() => {
         axios.get('http://localhost:3030/all')
             .then(
                 (res) => {
                     setData(res.data.result);
+                    setRecipeList(res.data.result);
                 });
-    }, []);
+                
+    }, []);     
+
+    const searchHandler = (e) => {
+        setSearch({
+            string: e.target.value,
+        });
+
+        let foundRecipe = data.filter(recipe => 
+            recipe.name.toLowerCase().includes(search.string.toLowerCase()));         
+             setRecipeList(foundRecipe);     
+    }
+
+    const dispalyRecipe = () =>{
+        console.log("recipeList", recipeList);
+        return recipeList.map((card) => (
+            <Card
+            key={card.id}
+            flag={card.flag}
+            image={card.image}
+            title={card.name}
+            link={`/SingleRecipe/${card.id}`}
+            />
+        ))
+    }
 
     return (
-        <div className={classes.bg} >
+        <div className={classes.recipes} >
                 <h1>Our Recipes</h1>
+                <div className={classes.search}>
+                    <label htmlFor="search">Search: </label>
+                    <input onChange={(e) => searchHandler(e)} name="search"></input>
+                </div>              
                 <div className={classes.cards}>
-                    {data.map((card) => (
-                        <Card
-                        key={card.id}
-                        flag={card.flag}
-                        image={card.image}
-                        title={card.name}
-                        link={`/SingleRecipe/${card.id}`}
-                        />
-                    ))}
+                    {dispalyRecipe()}
                 </div>
             </div>
     );
 };
+
 
 export default Recipes;
